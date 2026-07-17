14c9aff49576d32dbafcd3e903cbe5865fc401c6
Vlada Sokol <vlada.sokol.dev@gmail.com>
2026-07-17 03:05:47 -0400

    Code for module 1, tests pass

--- labs/week2_vision/module1_image_formation/tasks/image_formation.py
+++ labs/week2_vision/module1_image_formation/tasks/image_formation.py
@@ -25,8 +25,8 @@ def project_perspective(point_cam, f):
     X, Y, Z = point_cam
     ##################################
     #### START PUT CODE HERE #########
-    x = 0.0
-    y = 0.0
+    x = f*X/Z
+    y = f*Y/Z
     ###### END PUT CODE HERE #########
     ##################################
     return (x, y)
@@ -43,10 +43,10 @@ def meters_to_pixels(x, y, pixel_size, principal_point):
     cx, cy = principal_point
     ##################################
     #### START PUT CODE HERE #########
-    u = 0.0
-    v = 0.0
+    u = (x/pixel_size) + cx
+    v = (y/pixel_size) + cy
     ###### END PUT CODE HERE #########
-    ##################################
+    ##################################\
     return (u, v)
 
 
@@ -57,7 +57,8 @@ def intrinsic_matrix(fx, fy, cx, cy):
     """
     ##################################
     #### START PUT CODE HERE #########
-    K = np.eye(3)
+    K = np.array([[fx,0.0,cx],[0.0,fy,cy],[0.0,0.0,1.0]])
+
     ###### END PUT CODE HERE #########
     ##################################
     return K
@@ -73,8 +74,12 @@ def project_world_point(K, R, t, point_world):
     """
     ##################################
     #### START PUT CODE HERE #########
-    u = 0.0
-    v = 0.0
+    p_cam = R @ np.asarray(point_world, dtype=float)+ np.asarray(t, dtype=float)
+    p_homog = K @ p_cam
+    
+    u = p_homog[0]/p_homog[2]
+    v = p_homog[1]/p_homog[2]
+
     ###### END PUT CODE HERE #########
     ##################################
     return (u, v)
@@ -89,7 +94,9 @@ def apply_radial_distortion(x, y, k1, k2):
     """
     ##################################
     #### START PUT CODE HERE #########
-    factor = 1.0
+
+    r2 = (x**2) + (y**2)
+    factor = 1 + k1*r2 + k2*(r2**2)
     ###### END PUT CODE HERE #########
     ##################################
     return (x * factor, y * factor)
